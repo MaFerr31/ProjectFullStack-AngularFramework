6618ab3efb4f2b482ad5dc8440c9c37047d08ed0
Mariana Santos Ferreira <marianasferra31@gmail.com>
2025-03-06 15:36:10 -0300

    refact: componnets and submit

--- login-page/src/app/pages/login/login.component.ts
+++ login-page/src/app/pages/login/login.component.ts
@@ -1,22 +1,35 @@
 import { Component } from '@angular/core';
 import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
+import { Router } from 'express';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [DefaultLoginLayoutComponent,
-            ReactiveFormsModule
+  imports: [
+    DefaultLoginLayoutComponent,
+    ReactiveFormsModule,
+    PrimaryInputComponent,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
   loginForm!: FormGroup
-  constructor(){
+  constructor(
+  ){
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required, Validators.minLength(6)])
     })
+  }
+
+  submit(){
+    console.log(this.loginForm.value)
+  }
+
+  navigate(){
+    console.log(this.loginForm.value)
   }
 }
